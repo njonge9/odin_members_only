f7a5e1b3829029ec395c3a7d2d9411cdf4632600
njonge9 <frednjonge9@gmail.com>
2023-03-02 06:11:17 -0800

    Finish the association

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -65,6 +65,6 @@ class BlogsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def blog_params
-      params.require(:blog).permit(:title, :description)
+      params.require(:blog).permit(:title, :description, :user_id)
     end
 end
